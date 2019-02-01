29d80e6a7a15e46c47d1297650fa8307770f4127
Tom Holmes <tholmes59@gmail.com>
2019-02-01 22:03:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,9 @@ class Scraper
   
   def get_page
     doc= Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
- 
+ Scraper.new.get_page
   def get_courses
     
   end
